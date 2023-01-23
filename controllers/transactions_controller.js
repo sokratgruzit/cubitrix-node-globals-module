42688896513a017e20e7c98912877fb34d117ecf
MarkoPolo88 <talakhadzemariam@gmail.com>
2023-01-23 19:05:58 +0400

    get type_id

--- controllers/transactions_controller.js
+++ controllers/transactions_controller.js
@@ -1,11 +1,11 @@
 const transactions = require("../models/txs/transactions");
 const main_helper = require("../helpers/index");
+const transaction_types = require("../models/txs/transaction_types");
 
 // make_transaction
 async function make_transaction(req, res) {
   try {
     let { from, to, amount, tx_type, tx_hash, domination } = req.body;
-
     let tx_save = await transactions.create({
       from,
       to,
@@ -24,7 +24,21 @@ async function make_transaction(req, res) {
     return main_helper.error_message("error");
   }
 }
-
+async function get_tx_type(tx_type) {
+  try {
+    let type = await transaction_types.findOne({ name: tx_type }).exec();
+    if (type) {
+      return type;
+    } else {
+      await transaction_types.create({ name: tx_type }).exec();
+      type = await transaction_types.findOne({ name: tx_type }).exec();
+      return type;
+    }
+  } catch (e) {
+    console.log(e.message);
+    return main_helper.error_message("error");
+  }
+}
 module.exports = {
   get_account_balance,
   set_account_balance,
--- models/txs/transaction_types.js
+++ models/txs/transaction_types.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const transaction_types = new mongoose.Schema(
   {
     name: String,
+    tx_fee: Number,
   },
   {
     timestamps: true,
